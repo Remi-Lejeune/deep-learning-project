95c793651228e19a7829a34dca66a917593e0187
Marin Jaic <mjaic3@gmail.com>
2024-04-13 19:36:29 +0200

    Fixed

--- lcpfn/train_lcpfn.py
+++ lcpfn/train_lcpfn.py
@@ -41,7 +41,7 @@ def train_lcpfn(
     num_features = 1 #LALA
 
     ys = get_batch_func(
-        1, #HELLA HARDCODED
+        10_000,#HELLA
         seq_len,
         num_features,
         hyperparameters=hps,
